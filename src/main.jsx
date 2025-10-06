8d446470bb8a4cad5f1d10881eaf71093d114228
agus anwar <agus.anwar072@gmail.com>
2025-10-06 14:14:30 +0700

    test consol api in mainjsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,10 @@ import { WatchProvider } from "./componen/contexts/WatchContext.jsx";
 import { LoadingProvider } from "./componen/contexts/LoadingContext.jsx";
 import LoadingScreen from "./componen/sections/LoadingScreen.jsx";
 
+// 🔍 Tambahkan ini di sini
+console.log("🌍 ENV from Netlify:", import.meta.env);
+console.log("🔑 TMDB KEY:", import.meta.env.VITE_TMDB_API_KEY);
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
